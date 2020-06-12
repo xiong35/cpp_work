e976d2a5c82bea33866f2a757cfb6ea83a445477
xiong35 <2242787668@qq.com>
2020-06-12 09:28:52 +0800

    half done

--- Bank.h
+++ Bank.h
@@ -18,17 +18,12 @@ class YH
 public: //函数成员
 	YH();
 	void qu_account();	  //查询用户
-	void qu_server();	  //查询职员
 	void set_account();	  //银行开户
-	void set_register();  //职员注册
-	void del_account();	  //银行销户
 	void transfer(int);	  //转账
 	void enter_account(); //用户登录
-	void enter_server();  //职员登录
 	void exitYH();		  //退出系统
 	void show();		  //初始界面
 	void functionshow1(); //主菜单1
-	void functionshow2(); //主菜单2
 	void saveuser();	  //输入
 	void saveadm();		  //输出
 	void loaduser();
@@ -37,8 +32,6 @@ public: //函数成员
 protected:					   //数据成员
 	consumer *useraccount[20]; //账户数
 	static int useracnum;
-	Adm *admaccount[20]; //职员数
-	static int admacnum;
 };
 
 /*用户类*/
@@ -60,33 +53,12 @@ public:
 	void savemoney();		  //存钱
 	void fetchmoney();		  //取钱
 	void changemoney(double); //计算余额
-	void changepassword();	  //修改密码
 
 private:
 	int ID;		   //开户帐号
 	string passwd; //用户密码
 	string name;   //用户姓名
 	double money;  //开户金额
-};
-
-/*职员类*/
-class Adm : public YH
-{
-public:
-	friend class YH;
-	Adm(int id, const string = " ", const string = " ");
-	void setuserid(int);
-	int getuserid() const;
-	void setname(string);
-	string getname();
-	void setpassword(string);
-	string getpassword() const;
-	void display() const;
-	void changepasswd(); //修改密码
-private:
-	string password; //职员密码
-	string admname;	 //职员姓名
-	int userid;		 //职员卡号
 };
 
 #endif
